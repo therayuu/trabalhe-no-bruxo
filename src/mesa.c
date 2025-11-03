506309fbf74eba521da43c1bd69e8c7ffbca5a2f
theo-al <emailtheoprauerj@gmail.com>
2025-11-02 23:32:14 -0300

    mesa.c: struct carta
    ainda falta setar o tipo da carta no struct

--- src/mesa.c
+++ src/mesa.c
@@ -14,22 +14,29 @@ void mesa_setup(SDL_Renderer* ren) {
     mesa.init = true;
 }
 
+struct carta {
+    DragDropRect rect;
+    uint16_t cliques;
+
+    //! SDL_Texture* img;
+    //! SDL_Color* color;
+};
+
 enum tela mesa_loop(SDL_Renderer* ren, SDL_Event evt) {
     const int rw = W_WIDTH/10, rh = rw*3/2,
               sep = rw+pad, hmid = (W_HEIGHT-rh)/2;
-    static DragDropRect quadrados[] = {
-        { .r.x = sep*0, .r.y = hmid, .r.w=rw, .r.h=rh },
-        { .r.x = sep*1, .r.y = hmid, .r.w=rw, .r.h=rh },
-        { .r.x = sep*2, .r.y = hmid, .r.w=rw, .r.h=rh },
-        { .r.x = sep*3, .r.y = hmid, .r.w=rw, .r.h=rh },
-        { .r.x = sep*4, .r.y = hmid, .r.w=rw, .r.h=rh },
-        { .r.x = sep*5, .r.y = hmid, .r.w=rw, .r.h=rh },
-        { .r.x = sep*6, .r.y = hmid, .r.w=rw, .r.h=rh },
-        { .r.x = sep*7, .r.y = hmid, .r.w=rw, .r.h=rh },
-        { .r.x = sep*8, .r.y = hmid, .r.w=rw, .r.h=rh },
-        { .r.x = sep*9, .r.y = hmid, .r.w=rw, .r.h=rh },
+    static struct carta cartas[] = {
+        {{ .r.x = sep*0, .r.y = hmid, .r.w=rw, .r.h=rh }, .cliques = 0},
+        {{ .r.x = sep*1, .r.y = hmid, .r.w=rw, .r.h=rh }, .cliques = 0},
+        {{ .r.x = sep*2, .r.y = hmid, .r.w=rw, .r.h=rh }, .cliques = 0},
+        {{ .r.x = sep*3, .r.y = hmid, .r.w=rw, .r.h=rh }, .cliques = 0},
+        {{ .r.x = sep*4, .r.y = hmid, .r.w=rw, .r.h=rh }, .cliques = 0},
+        {{ .r.x = sep*5, .r.y = hmid, .r.w=rw, .r.h=rh }, .cliques = 0},
+        {{ .r.x = sep*6, .r.y = hmid, .r.w=rw, .r.h=rh }, .cliques = 0},
+        {{ .r.x = sep*7, .r.y = hmid, .r.w=rw, .r.h=rh }, .cliques = 0},
+        {{ .r.x = sep*8, .r.y = hmid, .r.w=rw, .r.h=rh }, .cliques = 0},
+        {{ .r.x = sep*9, .r.y = hmid, .r.w=rw, .r.h=rh }, .cliques = 0},
     };
-    static size_t clicks[LEN(quadrados)] = {0};
 
     enum tela prox_tela = MESA;
     switch (evt.type) {
@@ -39,23 +46,20 @@ enum tela mesa_loop(SDL_Renderer* ren, SDL_Event evt) {
 
       case SDL_USEREVENT: switch (evt.user.code) {
           case AUX_SURECLICKEVENT: {
-              clicks[LEN(clicks)-1] += 1;
+              cartas[LEN(cartas)-1].cliques += 1;
           } break;
 
           case AUX_TIMEOUTEVENT: {
               AUX_RenderClearColor(ren, BRANCO);
-              for (size_t i = 0; i < LEN(quadrados); i++) {
-                  const SDL_Color idle[] = {AZUL_BEBE, CINZA, AMARELO_M};
-
-                  const SDL_Color cor[DRAG_STATE_COUNT] = {
-                      clicks[i] ? idle[(clicks[i]-1) % LEN(idle)]
-                                : PRETO,
-                                  AZUL, VERMELHO_M
-                  };
-
-                  const uint8_t idx = quadrados[i].state;
-                  AUX_SetRenderDrawColor(ren, cor[idx]);
-                  SDL_RenderFillRect(ren, &quadrados[i].r);
+              for (size_t i = 0; i < LEN(cartas); i++) {
+                  const struct carta carta = cartas[i];
+                  const int estado = transmute(DragDropRect, carta).state;
+                  const SDL_Color cor = (estado == CLICKING) ?
+                      VERMELHO : (carta.cliques % 2) ? AZUL : PRETO
+                  ;
+                  AUX_SetRenderDrawColor(ren, cor);
+                  SDL_RenderFillRect(ren, (SDL_Rect*)&carta);
+                  //! if(carta.img) SDL_RenderCopy(ren, carta.img, NULL, &carta);
               }
 
               SDL_RenderPresent(ren);
@@ -63,10 +67,10 @@ enum tela mesa_loop(SDL_Renderer* ren, SDL_Event evt) {
       }
     }
 
-    for (size_t i = LEN(quadrados); i--; ) {
-        AUX_DragDropCancel(&quadrados[i], evt);
-        if (quadrados[i].state != UNCLICKED) {
-            AUX_ToEnd(quadrados, i); AUX_ToEnd(clicks, i); break;
+    for (size_t i = LEN(cartas); i--; ) {
+        AUX_DragDropCancel(&cartas[i].rect, evt);
+        if (cartas[i].rect.state != UNCLICKED) {
+            AUX_ToEnd(cartas, i); break;
         }
     }
     return prox_tela;
