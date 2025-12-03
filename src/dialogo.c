0a9c40c401c2f8d1a06b3a95311b806f45299712
theo-al <emailtheoprauerj@gmail.com>
2025-12-03 18:31:28 -0300

    diálogo: estilo
    nomes, constantes e funções auxiliares

--- src/dialogo.c
+++ src/dialogo.c
@@ -5,6 +5,9 @@
 
 #define TAM_FONTE 20
 
+#define FUNDO_NOME  CINZA_MEDIO
+#define FUNDO_TEXTO CINZA_ESCURO
+
 
 typedef struct dialog_option DialogueOption;
 typedef struct dialog_node   DialogueNode;
@@ -29,7 +32,7 @@ static struct estado_dialogo {
     size_t count;
     DialogueNode arvore[300];
 
-    size_t selected_option;
+    size_t opt_idx;
     DialogueNode* current;
     bool waiting_for_choice; //! esse campo não deveria existir
 
@@ -56,6 +59,7 @@ DialogueNode* createNode(const char* speaker, const char* text) {
 
 void addOption(DialogueNode* n, const char* text, DialogueNode* next) {
     assert(n->num_opts < LEN(n->options));
+
     size_t idx = n->num_opts++;
     n->options[idx].text = text;
     n->options[idx].next = next;
@@ -72,11 +76,12 @@ void loadDialogue() {
 }
 
 void renderText(SDL_Renderer* ren, TTF_Font* font, const char* text, int x, int y) {
-    SDL_Color white = {255, 255, 255, 255};
-    SDL_Surface* surf = TTF_RenderText_Blended(font, text, white);
-    SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, surf);
+    SDL_Surface* surf = TTF_RenderText_Blended(font, text, BRANCO);
+    SDL_Texture* tex  = SDL_CreateTextureFromSurface(ren, surf);
+
     SDL_Rect dst = {x, y, surf->w, surf->h};
     SDL_RenderCopy(ren, tex, NULL, &dst);
+
     SDL_FreeSurface(surf);
     SDL_DestroyTexture(tex);
 }
@@ -84,24 +89,23 @@ void renderText(SDL_Renderer* ren, TTF_Font* font, const char* text, int x, int 
 void renderDialog(SDL_Renderer* ren, TTF_Font* font) {
     DialogueNode* node = dialogo.current;
 
-    SDL_Rect nameBox = {TAM_FONTE*2, W_WIDTH/2, 10*TAM_FONTE, TAM_FONTE*2 + TAM_FONTE/2};
-    SDL_Rect textBox = {TAM_FONTE*2, nameBox.y + nameBox.h, W_WIDTH - (TAM_FONTE*4), TAM_FONTE*6};
+    SDL_Rect name = {TAM_FONTE*2, W_WIDTH/2, 10*TAM_FONTE, TAM_FONTE*2 + TAM_FONTE/2};
+    SDL_Rect text = {TAM_FONTE*2, name.y + name.h, W_WIDTH - (TAM_FONTE*4), TAM_FONTE*6};
+    AUX_SetRenderDrawColor(ren, FUNDO_TEXTO); SDL_RenderFillRect(ren, &text);
+    AUX_SetRenderDrawColor(ren, FUNDO_NOME);  SDL_RenderFillRect(ren, &name);
 
-    SDL_SetRenderDrawColor(ren, 50, 50, 50, 255); SDL_RenderFillRect(ren, &textBox);
-    SDL_SetRenderDrawColor(ren, 80, 80, 80, 255); SDL_RenderFillRect(ren, &nameBox);
-
-    renderText(ren, font, node->speaker, nameBox.x + 10, nameBox.y + 10);
-    renderText(ren, font, node->text,    textBox.x + 10, textBox.y + 10);
+    renderText(ren, font, node->speaker, name.x + 10, name.y + 10);
+    renderText(ren, font, node->text,    text.x + 10, text.y + 10);
 
     if (dialogo.waiting_for_choice) {
         for (size_t i = 0; i < node->num_opts; ++i) {
-            const char* prefix = (i == dialogo.selected_option ? "> " : "  ");
+            const char* prefix = (i == dialogo.opt_idx ? "> " : "  ");
             renderText(ren, font, prefix,
-                       textBox.x + TAM_FONTE,
-                       textBox.y + TAM_FONTE*3 + i*(TAM_FONTE+5));
+                       text.x + TAM_FONTE,
+                       text.y + TAM_FONTE*3 + i*(TAM_FONTE+5));
             renderText(ren, font, node->options[i].text,
-                       textBox.x + TAM_FONTE*2,
-                       textBox.y + TAM_FONTE*3 + i*(TAM_FONTE+5));
+                       text.x + TAM_FONTE*2,
+                       text.y + TAM_FONTE*3 + i*(TAM_FONTE+5));
         }
     }
 }
@@ -115,7 +119,7 @@ void dialogo_setup(SDL_Renderer* ren) {
     dialogo.font = TTF_OpenFont(ASSETS"tiny.ttf", TAM_FONTE);
     dialogo.current = &dialogo.arvore[0];
     dialogo.waiting_for_choice = false;
-    dialogo.selected_option = 0;
+    dialogo.opt_idx = 0;
     dialogo.count = 0;
 
     loadDialogue();
@@ -130,8 +134,8 @@ enum tela dialogo_loop(SDL_Renderer* ren, SDL_Event evt) {
 
           case SDLK_SPACE: case SDLK_RETURN: {
               if (dialogo.waiting_for_choice) {
-                  if (dialogo.selected_option < node->num_opts) {
-                      dialogo.current = node->options[dialogo.selected_option].next;
+                  if (dialogo.opt_idx < node->num_opts) {
+                      dialogo.current = node->options[dialogo.opt_idx].next;
                       dialogo.waiting_for_choice = false;
                   }
               } else {
@@ -145,12 +149,12 @@ enum tela dialogo_loop(SDL_Renderer* ren, SDL_Event evt) {
 
           //! puxar lógica do menu
           case SDLK_UP: if (dialogo.waiting_for_choice) {
-              dialogo.selected_option = (dialogo.selected_option - 1 + node->num_opts) % node->num_opts;
+              dialogo.opt_idx = (dialogo.opt_idx - 1 + node->num_opts) % node->num_opts;
           } break;
 
           //! puxar lógica do menu
           case SDLK_DOWN: if (dialogo.waiting_for_choice) {
-              dialogo.selected_option = (dialogo.selected_option + 1) % node->num_opts;
+              dialogo.opt_idx = (dialogo.opt_idx + 1) % node->num_opts;
           } break;
       } break;
 
